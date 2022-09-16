246aaa2f3f376ffd4c01892a87cd34d1cf429b4f
Steve Morillo <morillosteve01@gmail.com>
2022-09-16 14:52:05 +0800

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT ||  10001;
+const port = process.env.PORT ||  10000;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
